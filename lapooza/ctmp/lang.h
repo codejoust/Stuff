f1c8d60511b031191c7560e416efe6734d2fb495
Iain <info@codejoust.com>
2010-04-08 01:03:45 -0400

    adding lapooza\!

---
+++ lapooza/ctmp/lang.h
@@ -0,0 +1,48 @@
+                                                                                                #include <stdio.h>            
+/**----------      lapooza!  english computer language!                      **/         
+/**                                                                          **/            #define                    to_equal                             =
+/**     want the speed of C, but the ease of use and keywords of basic?      **/            #define                       is_equal                        ==
+/**        Term definition:                                                  **/            #define                   not_equal                         !=
+/**            Run a program... type: lapozza program.lpz                    **/            #define            are_equal
+/**            Begin a .lpz program with "Welcome"                           **/            #define what 
+/**            End it with Thanks                                            **/            #define works )==0){
+/**            Inbetween:                                                    **/            #define comparing strcmp(
+/**               Print to screen type - write "my message" please           **/            #define is );
+/**               To setup a variable: gimme my_var swell                    **/            #define if if(
+/**         More information in the demo file for now.                       **/                                                                                            #define otherwise } else {
+/**                                                                          **/                                                                                            #define ok }
+/**                                                                          **/                                                                                            #define then ){
+/**                                                                          **/                                                                                            #define Calculate printf("Calculated:\n%d\n", 
+/**                                                                          **/                                                                                            #define and +
+/**                                                                          **/                                                                                            #define Thanks  lexit: enter return(0); }
+/**                                                                          **/                                                                                            #define Welcome  int main(void){
+                                                                                            #define true 1
+                                                                                            #define you  true
+                                                                                            #define rock true
+                                                                                            #define minus -
+                                                                                            #define add +
+                                                                                            #define divided /
+                                                                                            #define by
+                                                                                            #define to 
+                                                                                            #define gimme char 
+                                                                                            #define swell [200];
+                                                                                            #define something (
+                                                                                            #define yeah );
+                                                                                            #define poot printf("\nplrrb\n");
+                                                                                            #define grab scanf("%s", 
+                                                                                            #define ask_for grab 
+                                                                                            #define write printf("%s\n", 
+                                                                                            #define with ,
+                                                                                            #define clear enter
+                                                                                            #define enter printf("\n");
+                                                                                            #define tis_all }
+                                                                                            #define please );
+                                                                                            #define thanks ;
+                                                                                            #define exit goto lexit;
+                                                                                            #define section
+                                                                                            #define end }
+                                                                                            #define also } else if                                                                                        
+                                                                                            #define something (  
+                                                                                            #define yeah ); 
+                                                                                            #define poot printf("\nplrrb\n");
+                                                                                            #define grab scanf("%s",  
